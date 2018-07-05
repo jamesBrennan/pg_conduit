4c0ce26d623b9415ee6519f72c7cb2998a45c255
James Brennan <james.brennan@educationsuperhighway.org>
2018-07-05 16:44:53 -0700

    Write coverage reports to /tmp on circle

--- spec/support/coverage.rb
+++ spec/support/coverage.rb
@@ -1,5 +1,12 @@
 require 'simplecov'
-SimpleCov.coverage_dir('/src/coverage')
+
+if ENV['CI'] == 'true'
+  SimpleCov.coverage_dir('/tmp/coverage')
+else
+  local_dir = File.join File.dirname(__FILE__), '..', '..', 'coverage'
+  SimpleCov.coverage_dir local_dir
+end
+
 SimpleCov.start do
   add_filter %r{_spec\.rb$}
 end
