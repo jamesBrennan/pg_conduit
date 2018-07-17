bbfeb63f7637e3cc3df141d8f95b73cf6e597fd4
James Brennan <james.brennan@educationsuperhighway.org>
2018-07-17 15:49:36 -0700

    Replace parallel_stream_reader internals with Parallelize

--- lib/pg_conduit/parallel_stream_reader.rb
+++ lib/pg_conduit/parallel_stream_reader.rb
@@ -6,9 +6,9 @@ module PgConduit
     # @param queue_max_size [Integer] How many rows should be stored in memory
     #   in the work queue.
     def initialize(query_stream, threads: 5, queue_max_size: 1000)
-      @queue = SizedQueue.new(queue_max_size)
-      @workers = threads
-      @stream = query_stream
+      @reader = Parallelize
+                  .new(max_concurrency: threads, max_messages: queue_max_size)
+                  .call(query_stream)
     end
 
     # Read A QueryStream and yield it's rows
@@ -16,45 +16,8 @@ module PgConduit
     # @yield [Hash] A single row from the QueryStream. Every row from the stream
     #   will be yielded but order is not guaranteed.
     def read(&callback)
-      reader  = read_stream(@stream)
-      workers = dispatch_workers(&callback)
-      reader.join
-      workers.each { |t| t.join }
+      @reader.each(&callback)
       :ok
-    end
-
-    private
-
-    def read_stream(query_stream)
-      Thread.new do
-        query_stream.each { |row| @queue << row }
-        @queue.close
-      end
-    end
-
-    def dispatch_workers(&callback)
-      (1..@workers).to_a.map { dispatch_worker(&callback) }
-    end
-
-    def dispatch_worker(&callback)
-      Thread.new do
-        loop do
-          continue = process_next(&callback)
-          break if @queue.closed? && !continue
-        end
-      end
-    end
-
-    def process_next(&callback)
-      continue = false
-      Thread.new do
-        row = @queue.deq
-        if row
-          callback.call row
-          continue = true
-        end
-      end.join
-      continue
     end
   end
 end
